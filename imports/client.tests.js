508311be24a5f1807d5fcad8e4beb075b048da17
Pier Roberto Lucisano <pierroberto.lucisano@gmail.com>
2017-11-20 20:07:13 +0100

    chore: add a test, not working yet

--- imports/client.tests.js
+++ imports/client.tests.js
@@ -1,20 +1,15 @@
 import { Meteor } from 'meteor/meteor';
 import { chai } from 'meteor/practicalmeteor:chai';
 
-if (Meteor.isServer) {
-  describe('Tasks', () => {
-    describe('methods', () => {
-      it('can delete owned task', () => {
-        chai.assert.equal(1+1,2);
-      });
-    });
-  });
-}
 if (Meteor.isClient) {
-  describe('Tasks', () => {
+  describe('Client Tasks', () => {
     describe('methods', () => {
       it('can delete owned task', () => {
-        chai.assert.equal(1+1,2);
+        Meteor.call('getVideoData','Qb8CZ4ktzFA', function (error, res) {
+          if (error) console.log('error', error)
+          console.log('ok', res);
+        })
+        //chai.assert.equal(1+1,2);
       });
     });
   });
